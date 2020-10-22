6548958c877b5bf5953ac1b1425800f4e2336329
A. S. Metcalf <70526588+asmetcalf@users.noreply.github.com>
2020-10-22 17:13:16 -0400

    Add project files.

---
+++ HR_CMS.WebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace HR_CMS.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ HR_CMS.WebAPI/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(HR_CMS.WebAPI.Startup))]
+
+namespace HR_CMS.WebAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
