5b539f2287d4d65335a971078cc7df005d5fdcca
A. S. Metcalf <70526588+asmetcalf@users.noreply.github.com>
2020-10-23 11:59:34 -0400

    built Position Models

--- HR_CMS.Models/05 Position/PositionCreate.cs
+++ HR_CMS.Models/05 Position/PositionCreate.cs
@@ -9,5 +9,15 @@ namespace HR_CMS.Models
 {
     public class PositionCreate
     {
+        [Required]
+        public string PositionTitle { get; set; }
+        [Required]
+        public int DeptId { get; set; } //If this causes issues, revisit whether this should be a DeptId or a Dept object
+        [Required]
+        public bool IsSupervisor { get; set; }
+        [Required]
+        public bool IsDirector { get; set; }
+        [Required]
+        public bool IsExecutive { get; set; }
     }
 }
--- HR_CMS.Models/05 Position/PositionDetail.cs
+++ HR_CMS.Models/05 Position/PositionDetail.cs
@@ -8,5 +8,12 @@ namespace HR_CMS.Models
 {
     public class PositionDetail
     {
+        public int PositionId { get; set; }
+        public string PositionTitle { get; set; }
+        public int DeptId { get; set; } //If this causes issues, revisit whether this should be a DeptId or a Dept object
+        public bool IsSupervisor { get; set; }
+        public bool IsDirector { get; set; }
+        public bool IsExecutive { get; set; }
+
     }
 }
