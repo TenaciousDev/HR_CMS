16ac19eef96abebeea9d244cf9a6f64d575e1504
TomislavKraljic <tkraljic0@gmail.com>
2020-10-23 12:06:15 -0400

    completed personnel model layer

--- HR_CMS.Models/01 Personnel/PersonnelDetail.cs
+++ HR_CMS.Models/01 Personnel/PersonnelDetail.cs
@@ -8,5 +8,20 @@ namespace HR_CMS.Models
 {
     public class PersonnelDetail
     {
+        public int PersonnelId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int SSN { get; set; }
+
+        public DateTimeOffset DOB { get; set; }
+
+        public DateTimeOffset DOH { get; set; }
+
+        public DateTimeOffset DOT { get; set; }
     }
 }
