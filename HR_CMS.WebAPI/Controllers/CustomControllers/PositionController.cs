7a470c9db8242e39a99113406ae3f00518285751
A. S. Metcalf <70526588+asmetcalf@users.noreply.github.com>
2020-10-26 16:23:34 -0400

    built out controllers for Dept and Position

--- HR_CMS.WebAPI/Controllers/CustomControllers/PositionController.cs
+++ HR_CMS.WebAPI/Controllers/CustomControllers/PositionController.cs
@@ -1,13 +1,60 @@
-﻿using System;
+﻿using HR_CMS.Models;
+using HR_CMS.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using System.Web.Http.Results;
 
 namespace HR_CMS.WebAPI.Controllers
 {
     public class PositionController : ApiController
     {
+        private PositionService CreatePositionService()
+        {
+            var createService = new PositionService();
+            return createService;
+        }
+        public IHttpActionResult Get()
+        {
+            PositionService positionService = CreatePositionService();
+            var positions = positionService.GetPositions();
+            return Ok(positions);
+        }
+        public IHttpActionResult Post(PositionCreate position)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var service = CreatePositionService();
+            if (!service.CreatePosition(position))
+            {
+                return InternalServerError();
+            }
+            return Ok();
+        }
+        public IHttpActionResult Get(int id)
+        {
+            PositionService positionService = CreatePositionService();
+            var position = positionService.GetPositionById(id);
+            return Ok(position);
+        }
+        public IHttpActionResult Put(PositionDetail position)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var service = CreatePositionService();
+            if (!service.UpdatePosition(position))
+            {
+                return InternalServerError();
+            }
+            return Ok();
+        }
     }
 }
