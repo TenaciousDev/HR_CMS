b2d6926e23432df6a181dfb663bb9cb58d6997f0
seanstephens967 <seanstephens967@gmail.com>
2020-10-26 11:45:41 -0400

    added contact service

--- HR_CMS.Services/Services/ContactService.cs
+++ HR_CMS.Services/Services/ContactService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HR_CMS.Data;
+using HR_CMS.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,84 @@ namespace HR_CMS.Services
 {
     public class ContactService
     {
+        public bool CreateContact(ContactCreate model)
+        {
+            var entity =
+                new 
+                Contact()
+                {
+                    PersonnelId = model.PersonnelId,
+                    PhoneNumber = model.PhoneNumber,
+                    Email = model.Email,
+                    Address = model.Address
+                };
 
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.ContactDbSet.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<ContactListItem> GetContacts()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .ContactDbSet
+                        .Select(
+                            e =>
+                                new ContactListItem
+                                {
+                                    PersonnelId = e.PersonnelId,
+                                    ContactId = e.ContactId,
+                                    PhoneNumber = e.PhoneNumber,
+                                    Email = e.Email,
+                                    Address = e.Address
+                                }
+                        );
+
+                return query.ToArray();
+            }
+        }
+
+        public ContactDetail GetContactById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .ContactDbSet
+                        .Single(e => e.ContactId == id);
+                return
+                    new ContactDetail
+                    {
+                        PersonnelId = entity.PersonnelId,
+                        ContactId = entity.ContactId,
+                        PhoneNumber = entity.PhoneNumber,
+                        Email = entity.Email,
+                        Address = entity.Address
+                    };
+            }
+        }
+
+        public bool UpdateNote(ContactDetail model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .ContactDbSet
+                        .Single(e => e.PersonnelId == model.PersonnelId);
+
+                entity.ContactId = model.ContactId;
+                entity.PhoneNumber = model.PhoneNumber;
+                entity.Email = model.Email;
+                entity.Address = model.Address;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
     }
 }
